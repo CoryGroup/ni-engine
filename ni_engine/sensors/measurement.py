974209e9dc40d932af5d3407b10d6f3299b5427e
Thomas Alexander <taalexander@mta.ca>
2013-06-21 16:36:59 -0400

    Added Measurement Class
    Have to add in Measurement functionality than good to go.

---
+++ ni_engine/sensors/measurement.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+class Measurement(object):
+
+	def __init__(self,sensorId,sensorCode,measurementName,value,time=datetime.now()):
+		self.sensorId = sensorId
+		self.sensorCode = sensorCode
+		self.measurementName = measurementName
+		self.value = value 
+		self.time = time
